87d65bdf6e01e0137f2c34088e0baf2151f36f93
sanketkedare <sanketkedare200@gmail.com>
2026-08-27 12:42:41 +0530

    🔧 config(ci): add github actions workflow & enable production console stripping ⚙️

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  compiler: {
+    removeConsole:
+      process.env.NODE_ENV === "production"
+        ? {
+            exclude: ["error"],
+          }
+        : false,
+  },
   images: {
     remotePatterns: [
       {
